316e58dd7b97cf837eeca24d6e748e9148759831
hugo hadfield <hadfield.hugo@gmail.com>
2021-06-11 15:11:18 +0100

    started work on making the klein and python se3 cayley the same

--- cayley.py
+++ cayley.py
@@ -1,8 +1,11 @@
 
-from clifford.g3 import *
+from clifford.pga import *
+
+def explicit_inverse(X):
+    return (~X)*(((X*~X).value[0] - (X*~X)(4))/((X*~X).value[0]**2))
 
 
-def cayley(phi):
+def cayley_so3(phi):
     """
     Implements the simplified so3 cayley map from bivectors to rotors
     """
@@ -10,11 +13,22 @@ def cayley(phi):
     return (1.0 + phi)*(1.0 + phi)/(1.0 - phi2.value[0]);
 
 
+
+def cayley_se3(phi):
+    """
+    Implements the simplified se3 cayley map from bivectors to rotors
+    """
+    phi2 = (phi*phi)
+    denominator = 1.0 - phi2.value[0]
+    return (1.0 + phi)*(1.0 + phi)*(( (1.0 - phi2.value[0]) + phi2(4))/(denominator*denominator));
+
+
+
 def cayley_unsimplified(phi):
     """
     Implements the bidirectional cayley map in its unsimplified form
     """
-    return (1 + phi)/(1 - phi)
+    return (1 + phi)*(1 - phi).inv()
 
 
 def cayley_kinematic(phi, omega):
@@ -25,7 +39,7 @@ def cayley_kinematic(phi, omega):
     return 0.25*(1.0 + phi)*omega*(1.0 - phi)
 
 
-if __name__ == "__main__":
+def test_so3():
     a = 0.1
     b = 0.2
     c = 0.3
@@ -37,7 +51,7 @@ if __name__ == "__main__":
     phi = a*(e23) + b*(e3*e1) + c*(e12)
     omega = d*(e23) + e*(e3*e1) + f*(e12)
 
-    R = cayley(phi);
+    R = cayley_so3(phi);
     print(R.value[0], R[2,3], R[3,1], R[1,2])
 
     Ralt = cayley_unsimplified(phi)
@@ -45,3 +59,40 @@ if __name__ == "__main__":
 
     kin_output = cayley_kinematic(phi, omega);
     print(kin_output[2,3], kin_output[3,1], kin_output[1,2])   
+
+
+def test_se3():
+    a = 0.1
+    b = 0.2
+    c = 0.3
+    d = 0.4
+    e = 0.5
+    f = 0.6
+
+    g = 0.1
+    h = 0.2
+    i = 0.3
+    j = -0.4
+    k = -0.5
+    l = -0.6
+
+    phi = a*(e23) + b*(e3*e1) + c*(e12) + g*e01 + h*e02 + i*e03
+    omega = d*(e23) + e*(e3*e1) + f*(e12) + j*e01 + k*e02 + k*e03
+
+    R = cayley_se3(phi)
+    print(R.value[0], R[2,3], R[3,1], R[1,2], R[0,1], R[0,2], R[0,3], R[0,1,2,3])
+
+    Ralt = cayley_unsimplified(phi)
+    print(Ralt.value[0], Ralt[2,3], Ralt[3,1], Ralt[1,2], Ralt[0,1], Ralt[0,2], Ralt[0,3], Ralt[0,1,2,3])
+
+    kin_output = cayley_kinematic(phi, omega);
+    print(kin_output[2,3], kin_output[3,1], kin_output[1,2], kin_output[0,1], kin_output[0,2], kin_output[0,3])   
+    print(kin_output)
+
+
+
+if __name__ == "__main__":
+    # print('so3')
+    # test_so3()
+    print('se3')
+    test_se3()
--- klein_ops.h
+++ klein_ops.h
@@ -17,6 +17,25 @@
     return out;
 }
 
+/// Line cast as motor
+[[nodiscard]] inline kln::motor KLN_VEC_CALL as_motor(kln::line b) noexcept
+{
+    kln::motor out;
+    out.p1_ = b.p1_;
+    out.p2_ = b.p2_;
+    return out;
+}
+
+/// Motor cast as line
+[[nodiscard]] inline kln::line KLN_VEC_CALL as_line(kln::motor b) noexcept
+{
+    kln::line out;
+    out.p1_ = b.p1_;
+    out.p2_ = b.p2_;
+    return out;
+}
+
+
 /// Branch scalar addition
 [[nodiscard]] inline kln::rotor KLN_VEC_CALL operator+(float a, kln::branch b) noexcept
 {
@@ -25,9 +44,31 @@
     return out;
 }
 
+/// Motor scalar addition
+[[nodiscard]] inline kln::motor KLN_VEC_CALL operator+(float a, kln::motor b) noexcept
+{
+    kln::motor out;
+    out.p1_ = _mm_add_ss(b.p1_, _mm_set_ss(a));
+    out.p2_ = b.p2_;
+    return out;
+}
+
+/// Line scalar addition
+[[nodiscard]] inline kln::motor KLN_VEC_CALL operator+(float a, kln::line b) noexcept
+{
+    return a + as_motor(b);
+}
+
 /// Branch times rotor
 [[nodiscard]] inline kln::rotor KLN_VEC_CALL operator*(kln::branch a, kln::rotor b) noexcept
 {
     kln::rotor out = as_rotor(a);
+    return out*b;
+}
+
+/// Line times rotor
+[[nodiscard]] inline kln::motor KLN_VEC_CALL operator*(kln::line a, kln::rotor b) noexcept
+{
+    kln::motor out = as_motor(a);
     return out*b;
 }
